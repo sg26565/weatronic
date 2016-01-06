55524670e4b902f241434e36c8c1aa249f747ea0
Oliver Treichel <oli@.treichels.de>
2016-01-06 19:38:05 +0100

    removed unnecessary XmlElement annotation

--- BAT64-Emulator-JDK/src/main/java/de/treichels/wea/bat64/config/vario/Motor.java
+++ BAT64-Emulator-JDK/src/main/java/de/treichels/wea/bat64/config/vario/Motor.java
@@ -1,15 +1,11 @@
 package de.treichels.wea.bat64.config.vario;
 
-import javax.xml.bind.annotation.XmlElement;
-
 import de.treichels.wea.bat64.config.Group;
 import de.treichels.wea.bat64.config.List;
 
 public class Motor extends Group {
 	public String Input;
-	@XmlElement(type = List.class)
 	public List<String> Item;
-	@XmlElement(type = List.class)
 	public List<String> LogFrequency;
 	public Short SensorType;
 }
